68757ad8fed514a3548297e8e68e411c4b292231
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 15:37:16 +0100

    Added a test that pawns can take pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+//import Board from '../board';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -10,17 +11,29 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = []
+
         if (this.player === Player.WHITE) {
-            if (location.row === 1) {
-                moves.push(Square.at(location.row + 2, location.col))
+            const block1WPlayer = board.getPiece(Square.at(location.row + 1, location.col))
+            const block2WPlayer = board.getPiece(Square.at(location.row + 2, location.col))
+            if (!block1WPlayer) {
+                if ((location.row === 1) && !block2WPlayer) {
+
+                    moves.push(Square.at(location.row + 2, location.col))
+                }
+
+                moves.push(Square.at(location.row + 1, location.col))
             }
-            moves.push(Square.at(location.row + 1, location.col))
         } else {
-            if (location.row === 6) {
-                moves.push(Square.at(location.row - 2, location.col))
+            const block1BPlayer = board.getPiece(Square.at(location.row - 1, location.col))
+            const block2BPlayer = board.getPiece(Square.at(location.row - 2, location.col))
+            if (!block1BPlayer) {
+                if ((location.row === 6) && !block2BPlayer) {
+                    moves.push(Square.at(location.row - 2, location.col))
+                }
+                moves.push(Square.at(location.row - 1, location.col))
             }
-            moves.push(Square.at(location.row - 1, location.col))
         }
         return moves
+
     }
 }
