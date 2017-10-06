47415a5e09590279515e41c75869469ad99c5888
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:52:02 +0100

    Made changes to rook.js

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player';
+
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +9,33 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        const moves = []
+
+
+        if (this.player === Player.WHITE) {
+
+            for (let i = 0; i < 8; i++) {
+
+                if (i != location.row) {
+                    moves.push(Square.at(i, location.col));
+                }
+                if (i != location.col) {
+                    moves.push(Square.at(location.row, i));
+                }
+            }
+        } else {
+            for (let i = 0; i < 8; i++) {
+                if (i != location.row) {
+                    moves.push(Square.at(i, location.col));
+                }
+                if (i != location.col) {
+                    moves.push(Square.at(location.row, i));
+                }
+            }
+
+        }
+
+        return moves
     }
 }
