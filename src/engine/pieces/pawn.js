35f01132c0ff7c20f59bc0e976deead8a9f58ce6
Prasanti <nalla.prasanti@gmail.com>
2023-01-24 09:52:40 +0000

    made changes to pawn.js to take off opponent player if any daigonally and checking if it King or not

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,5 +1,6 @@
 import Player from '../player';
 import Square from '../square';
+import King from './king';
 import Piece from './piece';
 //import Board from '../board';
 
@@ -13,19 +14,43 @@ export default class Pawn extends Piece {
         const moves = []
 
         if (this.player === Player.WHITE) {
+            if (location.row === 7) {
+                return moves;
+            }
             const block1WPlayer = board.getPiece(Square.at(location.row + 1, location.col))
             const block2WPlayer = board.getPiece(Square.at(location.row + 2, location.col))
+            const diagonalWLeftPlayer = board.getPiece(Square.at(location.row + 1, location.col - 1));
+            const diagonalWRightPlayer = board.getPiece(Square.at(location.row + 1, location.col + 1));
+            //console.log("opponent player is " + diagonalWLeftPlayer);
+            if (diagonalWLeftPlayer && diagonalWLeftPlayer.player === Player.BLACK && diagonalWLeftPlayer.constructor.name !== 'King') {
+                moves.push(Square.at(location.row + 1, location.col - 1))
+            }
+            else if (diagonalWRightPlayer && diagonalWRightPlayer.player === Player.BLACK && diagonalWRightPlayer.constructor.name !== 'King') {
+                moves.push(Square.at(location.row + 1, location.col + 1))
+            }
             if (!block1WPlayer) {
                 if ((location.row === 1) && !block2WPlayer) {
-
                     moves.push(Square.at(location.row + 2, location.col))
                 }
 
-                moves.push(Square.at(location.row + 1, location.col))
+            } moves.push(Square.at(location.row + 1, location.col))
+        }
+        else {
+            console.log(this.player)
+            if (location.row === 0) {
+                return moves;
             }
-        } else {
             const block1BPlayer = board.getPiece(Square.at(location.row - 1, location.col))
             const block2BPlayer = board.getPiece(Square.at(location.row - 2, location.col))
+            const diagonalBLeftPlayer = board.getPiece(Square.at(location.row - 1, location.col + 1));
+            const diagonalBRightPlayer = board.getPiece(Square.at(location.row - 1, location.col - 1));
+
+            if (diagonalBLeftPlayer && diagonalBLeftPlayer.player === Player.WHITE && diagonalBLeftPlayer.constructor.name !== 'King') {
+                moves.push(Square.at(location.row - 1, location.col + 1))
+            }
+            else if (diagonalBRightPlayer && diagonalBRightPlayer.player === Player.WHITE && diagonalBRightPlayer.constructor.name !== 'King') {
+                moves.push(Square.at(location.row - 1, location.col - 1))
+            }
             if (!block1BPlayer) {
                 if ((location.row === 6) && !block2BPlayer) {
                     moves.push(Square.at(location.row - 2, location.col))
--- src/engine/player.js
+++ src/engine/player.js
@@ -4,3 +4,4 @@ const Player = Object.freeze({
 });
 
 export default Player;
+
