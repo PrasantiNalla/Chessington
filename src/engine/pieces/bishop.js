15debe76df2f79fce914da3aa697bb0e4225cc1e
Prasanti <nalla.prasanti@gmail.com>
2023-01-24 22:08:13 +0000

    all bishops conditions met

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -26,8 +26,7 @@ export default class Bishop extends Piece {
         const moves = []
         let col = location.col;
         let row = location.row;
-        // bishop going up-right 
-
+        // bishop going up-right
         for (let i = location.row + 1; i <= 7; i++) {
             col++;
             if (col <= 7) {
@@ -47,51 +46,42 @@ export default class Bishop extends Piece {
 
             }
         }
-
         // bishop going up-left
-        //3, 2=> 4, 1=> 5, 0       2,6 => 3,5 => 4,4 => 5,3=>6,2=>7,1
         col = location.col;
         for (let i = location.row + 1; i <= 7; i++) {
             col--;
             if (col >= 0) {
-
                 const nextPlayer = board.getPiece(Square.at(i, col))
-                let counter = 0;
-                //counter++;
                 if (nextPlayer === undefined) {
-                    moves.push(Square.at(location.row + i, col));
+                    moves.push(Square.at(i, col));
                 }
                 else if (nextPlayer.player !== this.player && nextPlayer.constructor.name !== 'King') {
-                    moves.push(Square.at(location.row + i, col));
+                    moves.push(Square.at(i, col));
                     break;
                 }
                 else if (nextPlayer.player === this.player) {
                     break;
                 }
             }
-            //3,5 => 2,6 => 1,7
-            //6,1 => 5,2 =>4,3 => 3,4 => 2,5 => 1,6 => 0,7
-            // bishop trying move down-right
-            
-            for (let i = location.col + 1; i <= 7; i++) {
-                row--;
-                if (row >= 0) {
-                    const nextPlayer = board.getPiece(Square.at(row, i))
-                    if (nextPlayer === undefined) {
-                        moves.push(Square.at(location.row + i, location.col - i));
-                    }
-                    else if (nextPlayer.player !== this.player && nextPlayer.constructor.name !== 'King') {
-                        moves.push(Square.at(location.row + i, location.col - i));
-                        break;
-                    }
-                    else if (nextPlayer.player === this.player) {
-                        break;
-                    }
+        }
+        // bishop trying move down-right
+        for (let i = location.col + 1; i <= 7; i++) {
+            row--;
+            if (row >= 0) {
+                // console.log("i'm updating" + row, i)
+                const nextPlayer = board.getPiece(Square.at(row, i))
+                if (nextPlayer === undefined) {
+                    moves.push(Square.at(row, i));
+                }
+                else if (nextPlayer.player !== this.player && nextPlayer.constructor.name !== 'King') {
+                    moves.push(Square.at(row, i));
+                    break;
+                }
+                else if (nextPlayer.player === this.player) {
+                    break;
                 }
             }
-
-
-        }//5,6  => 4,5 => 3,4 => 2,3 => 1,2 => 0,1
+        }
         // bishop trying move down-left
         row = location.row;
         for (let i = location.col - 1; i >= 0; i--) {
--- tests/engine/pieces/bishop.spec.js
+++ tests/engine/pieces/bishop.spec.js
@@ -19,9 +19,9 @@ describe('Bishop', () => {
 
         const expectedMoves = [
             // Forwards diagonal
-            Square.at(0, 1), Square.at(1, 2), Square.at(3, 4), Square.at(4, 5), Square.at(5, 6), Square.at(6, 7),
+           Square.at(0, 1), Square.at(1, 2), Square.at(3, 4), Square.at(4, 5), Square.at(5, 6), Square.at(6, 7),
             // Backwards diagonal
-            Square.at(0, 5), Square.at(1, 4), Square.at(3, 2), Square.at(4, 1), Square.at(5, 0)
+           Square.at(0, 5), Square.at(1, 4),Square.at(3, 2), Square.at(4, 1), Square.at(5, 0)
         ];
 
         moves.should.deep.include.members(expectedMoves);
