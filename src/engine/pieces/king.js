3003a5512747c405e58b984c2b49d8ff7e3ca1f6
Prasanti <nalla.prasanti@gmail.com>
2023-01-25 16:44:20 +0000

    all tests working.Final changes made

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,86 +1,35 @@
 import Piece from './piece';
 import Square from '../square';
-import Player from '../player';
-import Board from '../board';
+
 
 export default class King extends Piece {
     constructor(player) {
         super(player);
     }
-
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = []
         let row = location.row;
         let col = location.col;
+        function getKingMoves(row, col) {
+            if (row <= 7 && row >= 0 && col <= 7 && col >= 0) {
+                const nextSquare = board.getPiece(Square.at(row, col));
+                if (nextSquare === undefined) {
+                    moves.push(Square.at(row, col));
+                } else if (nextSquare.player !== this.player && nextSquare.constructor.name !== 'King') {
+                    moves.push(Square.at(row, col));
+                }
+            }
+        }
+        getKingMoves(row, col + 1);
+        getKingMoves(row, col - 1);
+        getKingMoves(row + 1, col - 1);
+        getKingMoves(row + 1, col + 1);
+        getKingMoves(row + 1, col);
+        getKingMoves(row - 1, col - 1);
+        getKingMoves(row - 1, col + 1);
+        getKingMoves(row - 1, col);
 
-        const nextPlayerSameRow1 = board.getPiece(Square.at(row, col + 1));
-        if (col + 1 <= 7) {
-            if (nextPlayerSameRow1 === undefined) {
-                moves.push(Square.at(row, col + 1));
-            } else if (nextPlayerSameRow1.player !== this.player && nextPlayerSameRow1.constructor.name !== 'King') {
-                moves.push(Square.at(row, col + 1));
-            }
-        }
-        if ((col - 1) >= 0) {
-            const nextPlayerSameRow2 = board.getPiece(Square.at(row, col - 1));
-            if (nextPlayerSameRow2 === undefined) {
-                moves.push(Square.at(row, col - 1));
-            } else if (nextPlayerSameRow2.player !== this.player && nextPlayerSameRow2.constructor.name !== 'King') {
-                moves.push(Square.at(row, col - 1));
-            }
-        }
-        if (row + 1 <= 7 && col - 1 >= 0) {
-            const nextPlayerFrontRow1 = board.getPiece(Square.at(row + 1, col - 1));
-            if (nextPlayerFrontRow1 === undefined) {
-                moves.push(Square.at(row + 1, col - 1));
-            } else if (nextPlayerFrontRow1.player !== this.player && nextPlayerFrontRow1.constructor.name !== 'King') {
-                moves.push(Square.at(row + 1, col - 1));
-            }
-        }
-        if (row + 1 <= 7 && col + 1 <= 7) {
-            const nextPlayerFrontRow2 = board.getPiece(Square.at(row + 1, col + 1));
-            if (nextPlayerFrontRow2 === undefined) {
-
-                moves.push(Square.at(row + 1, col + 1));
-            } else if (nextPlayerFrontRow2.player !== this.player && nextPlayerFrontRow2.constructor.name !== 'King') {
-                moves.push(Square.at(row + 1, col + 1));
-            }
-        }
-        if (row + 1 <= 7) {
-            const nextPlayerFrontRow3 = board.getPiece(Square.at(row + 1, col));
-            if (nextPlayerFrontRow3 === undefined) {
-
-                moves.push(Square.at(row + 1, col));
-            } else if (nextPlayerFrontRow3.player !== this.player && nextPlayerFrontRow3.constructor.name !== 'King') {
-                moves.push(Square.at(row + 1, col));
-            }
-        }
-        if (row - 1 >= 0 && col - 1 >= 0) {
-            const nextPlayerBackRow1 = board.getPiece(Square.at(row - 1, col - 1));
-            if (nextPlayerBackRow1 === undefined) {
-                moves.push(Square.at(row - 1, col - 1));
-            } else if (nextPlayerBackRow1.player !== this.player && nextPlayerBackRow1.constructor.name !== 'King') {
-                moves.push(Square.at(row - 1, col - 1));
-            }
-        }
-        if (row - 1 >= 0 && col + 1 <= 7) {
-            const nextPlayerBackRow2 = board.getPiece(Square.at(row - 1, col + 1));
-            if (nextPlayerBackRow2 === undefined || nextPlayerBackRow2.player !== this.player) {
-                moves.push(Square.at(row - 1, col + 1));
-            } else if (nextPlayerBackRow2.player !== this.player && nextPlayerBackRow2.constructor.name !== 'King') {
-                moves.push(Square.at(row - 1, col + 1));
-            }
-        }
-        if(row-1 >=0){
-        const nextPlayerBackRow3 = board.getPiece(Square.at(row - 1, col));
-        if (nextPlayerBackRow3 === undefined || nextPlayerBackRow3.player !== this.player) {
-
-            moves.push(Square.at(row - 1, col));
-        } else if (nextPlayerBackRow3.player !== this.player && nextPlayerBackRow3.constructor.name !== 'King') {
-            moves.push(Square.at(row - 1, col));
-        }
-    }
         return moves;
     }
 }
--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -3,66 +3,42 @@ import Square from '../square';
 import Player from '../player';
 import Board from '../board';
 
+
 export default class Knight extends Piece {
     constructor(player) {
         super(player);
     }
-
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = []
-        let col = location.col;
-        let row = location.row;
-
+        function getKnightMoves(row, col) {
+            if (row <= 7 && row >= 0 && col >= 0 && col <= 7) {
+                console.log(row, col)
+                const nextSquare = board.getPiece(Square.at(row, col));
+                if (nextSquare === undefined) {
+                    moves.push(Square.at(row, col));
+                }
+                else if (nextSquare.player !== this.player && nextSquare.constructor.name !== 'King') {
+                    moves.push(Square.at(row, col));
+                }
+            }
+        }
         // knight jumps up-right (4,4) => (6,5)
-        if (row + 2 <= 7) {
-
-            const nextUpRightSquare = board.getPiece(Square.at(row + 2, col + 1));
-            if (nextUpRightSquare === undefined) {
-                moves.push(Square.at(row + 2, col + 1));
-            }
-            else if (nextUpRightSquare.player !== this.player && nextUpRightSquare.constructor.name !== 'King') {
-                moves.push(Square.at(row + 2, col + 1));
-            }
-        }
-
+        getKnightMoves(location.row + 2, location.col + 1);
         //knight jumps up-right corner (4,4) => (5,6)
-        if (col + 2 <= 7) {
-
-            const nextUpRightSquare = board.getPiece(Square.at(row + 1, col + 2));
-            if (nextUpRightSquare === undefined) {
-                moves.push(Square.at(row + 1, col + 2));
-            }
-            else if (nextUpRightSquare.player !== this.player && nextUpRightSquare.constructor.name !== 'King') {
-                moves.push(Square.at(row + 1, col + 2));
-            }
-        }
-        // knight jumps up-left (4,4) => (6,3)
-        if (row - 2 >= 0) {
-            console.log("Hello2" + col + row)
-            const nextUpLeftSquare = board.getPiece(Square.at(row + 2, col - 1));
-            if (nextUpLeftSquare === undefined) {
-                moves.push(Square.at(row + 2, col - 1));
-            }
-            else if (nextUpLeftSquare.player !== this.player && nextUpLeftSquare.constructor.name !== 'King') {
-                moves.push(Square.at(row + 2, col - 1));
-            }
-        }
-        // knight jumps up-left corner (4,4) => (5,2)
-        if (col - 2 >= 0) {
-
-            const nextUpRightSquare = board.getPiece(Square.at(row + 1, col - 2));
-            if (nextUpRightSquare === undefined) {
-                moves.push(Square.at(row + 1, col - 2));
-            }
-            else if (nextUpRightSquare.player !== this.player && nextUpRightSquare.constructor.name !== 'King') {
-                moves.push(Square.at(row + 1, col - 2));
-            }
-        }
+        getKnightMoves(location.row + 1, location.col + 2);
+        // // knight jumps up-left (4,4) => (6,3)
+        getKnightMoves(location.row + 2, location.col - 1);
+        // // knight jumps up-left corner (4,4) => (5,2)
+        getKnightMoves(location.row + 1, location.col - 2);
         // knight jumps down-right (4,4) => (2,5)
+        getKnightMoves(location.row - 2, location.col + 1);
         // knight jumps down-right corner (4,4) => (3,6)
+        getKnightMoves(location.row - 1, location.col + 2);
         // knight jumps down -left (4,4) => (2,3)
+        getKnightMoves(location.row - 2, location.col - 1);
         // knight jumps down-right corner (4,4) => (3,2)
+        getKnightMoves(location.row - 1, location.col - 2);
         return moves;
     }
 }
